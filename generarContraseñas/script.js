1a987342f6af899325f8154c200f26e487a15fc6
Ivan Lezcano <lezcamati@gmail.com>
2024-01-27 20:29:38 -0300

    generadorContraseñas 100%

--- generarContraseñas/script.js
+++ generarContraseñas/script.js
@@ -1,21 +1,35 @@
+const inputLength = document.getElementById('inputLenght');
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
+const generatedPassword = document.getElementById('generatedPassword');
+const btnGenerate = document.getElementById('btnGenerate');
+
+const generatedPass = (base, length) => {
+    let password = '';
+    for (let x = 0; x < length; x++){
+        let random = Math.floor(Math.random() * base.length);
+        password += base.charAt(random);
+    };
+    return password;
+};
+
+
 const generate = () =>{
-    let lenght = parseInt(inputLength.value);
+    let length = parseInt(inputLength.value);
 
     let base = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numbers = '0123456789';
     const symbols = '.,*{}!"#$%&/()=?¡¿@<>'
 
-    let generatedPassword = document.getElementById('generatedPassword');
+    // let generatedPassword = document.getElementById('generatedPassword');
 
-    // if (checkbox1.checked) base+=numbers;
-    // if (checkbox2.checked) base+=symbols ;
+    if (checkbox1.checked) base+=numbers;
+    if (checkbox2.checked) base+=symbols;
 
-    generatedPassword.innerText = 'AAA';
+    generatedPassword.innerText = generatedPass(base, length);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-    let btnGenerate = document.getElementById('btnGenerate');
-
     btnGenerate.addEventListener('click', ()=>{
         generate();
     });
